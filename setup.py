90ce80693aa91072a1de47b18b541451d3b7fa11
Ivan Vrbovčan <ivrbovcan@gmail.com>
2023-03-26 09:28:07 +0200

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Nomendi6/create_doc',
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
 )
